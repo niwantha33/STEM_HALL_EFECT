9ec043bf1053c890d6b2886ec7e8dde3cea2e6bd
niwantha33 <niwantha33@gmail.com>
2020-08-24 21:55:11 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ long hallValue= hallRead();
+ Serial.println(hallValue);
+ delay(2000);
 }
